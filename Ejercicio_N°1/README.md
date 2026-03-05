183e30723cbca6d2825c9704737a059f5cd14424
everfigueroa-alt <everfigueroa@colegioguanenta.edu.co>
2026-03-05 15:29:30 -0500

    se actualizo en readme con analisis y diseño

---
+++ Ejercicio_N°1/README.md
@@ -0,0 +1,5 @@
+# Validador de Passwords
+Programa en python para verificar si la contraseña es correcta 
+
+## Diseño
+![diagrama de flujo](diagrama.png)
---
+++ Ejercicio_N°1/Validador_de_passwords.py
@@ -0,0 +1,10 @@
+contraseña = ""
+
+while contraseña != "python123":
+    contraseña=input("ingrese la contraseña: ")
+
+    if contraseña != "python123":
+        print("Error")
+
+
+print("Acceso Concedido")
